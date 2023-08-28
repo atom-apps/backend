eee006a83a884bc9c1bdc6371bc48ae282399b1e
rogeecn <rogeecn@qq.com>
2023-08-28 18:19:01 +0800

    sync issues

--- src/views/users/view/index.vue
+++ src/views/users/view/index.vue
@@ -1,12 +1,50 @@
 <template>
-  <div class="mx-5 my-5 mt-0">
-    <Breadcrumb :items="['users', 'users.list', 'users.view','title']" />
-    <a-card class="general-card" title="用户查看">
-   
-    </a-card>
-  </div>
+  <a-page-header title="用户管理" subtitle="用户详情" :show-back="false" class="bg-white">
+    <template #breadcrumb>
+      <a-breadcrumb>
+        <a-breadcrumb-item>用户管理</a-breadcrumb-item>
+        <a-breadcrumb-item>用户详情</a-breadcrumb-item>
+      </a-breadcrumb>
+    </template>
+
+    <template #extra>
+      <a-space class="flex justify-end items-center">
+        <a-button>编辑</a-button>
+        <a-button>删除</a-button>
+      </a-space>
+    </template>
+  </a-page-header>
+
+  <a-card class="m-5">
+    <a-descriptions :data="renderData" :column="1" align="right" size="mini"/>
+  </a-card>
 </template>
 
 <script lang="ts" setup>
 
+import { DescData } from '@arco-design/web-vue';
+
+
+const renderData = [
+  {
+    label: "Username",
+    value: "Username",
+  },
+  {
+    label: 'Certification',
+    value: "Cert"
+  },
+  {
+    label: 'AccountID',
+    value: 'AccountID',
+  },
+  {
+    label: 'Phone',
+    value: "Phone",
+  },
+  {
+    label: 'RegisterAt',
+    value: "RegisterAt",
+  },
+] as DescData[];
 </script>
